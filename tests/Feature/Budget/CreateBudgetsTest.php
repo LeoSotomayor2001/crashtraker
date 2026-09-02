7d85c9c842e9afd34b4d3bb13a75dd5647e6680e
Leodomi Sotomayor <leodomi15@gmail.com>
2026-09-01 20:50:05 -0400

    Add tests for budget creation: validate empty form submission, check active session requirement, and ensure budget assignment to authenticated user

---
+++ tests/Feature/Budget/CreateBudgetsTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Models\Budget;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('validar cuando se envia form vacio', function () {
+    $user = User::factory()->create([
+        'email_verified_at' => now()
+    ]);
+
+    $response = $this->actingAs($user)
+        ->from(route('budgets.create'))->post(route('budgets.store'), [
+            'name' => '',
+            'amount' => '',
+            'type' => ''
+        ]);
+
+    $response->assertRedirect(route('budgets.create'));
+    $response->assertSessionHasErrors(['name', 'amount', 'type']);
+});
+
+
+it('no le permite crear un budget a un usuario que no tiene sesion activa', function(){
+    $response= $this->post(route('budgets.store'),[
+        'name' => 'boda',
+        'amount' => 222,
+        'type' => 'goal'
+    ]);
+
+    $response->assertRedirect(route('login'));
+});
+
+it('un presupuesto creado se asigna al usuario autenticado', function (){
+    $user = User::factory()->create([
+        'email_verified_at' => now()
+    ]);
+
+    $this->actingAs($user)->post(route('budgets.store',[
+        'name' => 'boda',
+        'amount' => 222,
+        'type' => 'goal'
+    ]));
+
+    $budget=Budget::first();
+    expect($budget->user_id)->toBe($user->id);
+});
